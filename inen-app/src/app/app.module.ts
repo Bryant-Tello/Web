87b08965c67d4b32c9e0bd7cc730fe2aaae7c30a
carmensalinas <carmen12.salinas@gmail.com>
2022-04-03 17:21:08 -0500

    instalacion de angular material

--- inen-app/src/app/app.module.ts
+++ inen-app/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -10,6 +9,8 @@ import { SeguimientoPacienteComponent } from './pages/seguimiento-paciente/segui
 import { DirectorioComponent } from './pages/directorio/directorio.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { HomeComponent } from './pages/home/home.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,9 @@ import { HomeComponent } from './pages/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
