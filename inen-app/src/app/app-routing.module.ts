8c306e2f6fc3d33a66b60c5851cad93843ad7a8d
carmensalinas <carmen12.salinas@gmail.com>
2022-04-03 16:36:02 -0500

    creacion de home y menu

--- inen-app/src/app/app-routing.module.ts
+++ inen-app/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DirectorioComponent } from './pages/directorio/directorio.component';
+import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { MedicosComponent } from './pages/medicos/medicos.component';
-import { MenuComponent } from './pages/menu/menu.component';
 import { PacientesComponent } from './pages/pacientes/pacientes.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { SeguimientoPacienteComponent } from './pages/seguimiento-paciente/seguimiento-paciente.component';
@@ -12,7 +12,7 @@ const routes: Routes = [
   {path: 'directorio', component: DirectorioComponent},
   {path: 'login', component: LoginComponent },
   {path: 'medicos', component: MedicosComponent},
-  {path: 'menu', component: MenuComponent},
+  {path : 'home', component: HomeComponent},
   {path: 'pacientes', component: PacientesComponent},
   {path: 'perfil', component: PerfilComponent},
   {path: 'seguimiento-paciente', component: SeguimientoPacienteComponent}
